05c971042e05d512622e97a0d111d7df656329a7
Nolan Lawson <nolan@nolanlawson.com>
2019-08-25 15:24:28 -0700

    finish repro

---
+++ README.md
@@ -0,0 +1,10 @@
+Reproduce tesseract.js fake image issue
+====
+
+Usage:
+
+    npm install
+    npm run build
+    npm start
+
+Then go to localhost:8080
---
+++ src.js
@@ -0,0 +1,23 @@
+import { TesseractWorker } from 'tesseract.js'
+
+(async () => {
+  const worker = new TesseractWorker({
+    workerPath: `/worker.min.js`,
+    langPath: `/`,
+    corePath: `/tesseract-core.wasm.js`
+  })
+
+  try {
+    const response = await worker.recognize('/fake.png')
+    console.log('resonse', response)
+  } catch (err) {
+    console.err('error', err)
+  }
+
+  try {
+    const response = await worker.recognize('/testocr.png')
+    console.log('response', response)
+  } catch (err) {
+    console.err('error', err)
+  }
+})()
